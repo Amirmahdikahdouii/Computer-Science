0884b6f9b3c2ce2fbda9efc259e420addd6db849
amirmahdikahdouii <amirmahdikahdooi@gmail.com>
2024-01-13 23:38:33 +0330

    Stack Implementation with python added,/bin/python3 /home/amir/Desktop/Computer-Science/DataStuctures/Stack/Implementation/stack.py

---
+++ DataStuctures/Stack/Implementation/Tests/test_stack.py
@@ -0,0 +1,50 @@
+import sys
+sys.path.append("../")
+
+from unittest import TestCase
+from stack import Stack
+
+class StackModuleTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        stack = Stack()
+        empty_stack = Stack()
+        stack.push(3)
+        stack.push(2)
+        stack.push(1)
+        cls.stack = stack
+        cls.empty_stack = empty_stack
+    
+    def test_empty_stack(self):
+        # Test stack methods with empty stack
+        with self.assertRaises(IndexError):
+            self.empty_stack.pop()
+        self.assertIsNone(self.empty_stack.peek())
+        self.assertEqual(len(self.empty_stack), 0)
+        self.assertEqual(self.empty_stack.__str__(), "None")
+        
+        # Add new node to stack and test the methods
+        self.empty_stack.push(1)
+        self.assertEqual(len(self.empty_stack), 1)
+        self.assertEqual(self.empty_stack.__str__(), "1 -> None")
+        self.assertIsNotNone(self.empty_stack.peek())
+        self.assertEqual(self.empty_stack.peek(), 1)
+        
+        # Delete Node from stack and test empty stack again
+        self.empty_stack.__del__()
+        self.assertEqual(len(self.empty_stack), 0)
+        self.assertEqual(self.empty_stack.__str__(), "None")
+        with self.assertRaises(IndexError):
+            self.empty_stack.pop()
+        self.assertIsNone(self.empty_stack.peek())
+            
+    def test_stack(self):
+        self.assertEqual(len(self.stack), 3)
+        self.assertEqual(self.stack.peek(), 1)
+        self.assertIn("1 -> 2 -> 3", self.stack.__str__())
+        self.assertEqual(self.stack.pop(), 1)
+        self.assertEqual(len(self.stack), 2)
+        
+        # Empty the stack
+        self.stack.__del__()
+        self.assertEqual(len(self.stack), 0)
---
+++ DataStuctures/Stack/Implementation/stack.py
@@ -0,0 +1,70 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+    def pop(self):
+        """
+        Remove first item from stack
+        """
+        if self.top is None:
+            raise IndexError("Stack is empty")
+        node = self.top
+        data = node.data
+        self.top = node.next
+        del node
+        return data
+
+    def peek(self):
+        """
+        Return first stack node data
+        """
+        if self.top is None:
+            return None
+        return self.top.data
+
+    def push(self, data):
+        node = Node(data)
+        node.next = self.top
+        self.top = node
+
+    def __len__(self):
+        """
+        Return Length of the stack
+        """
+        if self.top is None:
+            return 0
+        temp = self.top
+        i = 1
+        while temp.next is not None:
+            i += 1
+            temp = temp.next
+        return i
+    
+    def __str__(self):
+        """
+        Return str object of stack nodes data.
+        If stack is empty return str obj equal to "None".
+        Example:
+        # If We have a stack that has 3 items below, the output will be:
+        1 -> 2 -> 3 -> None
+        """
+        output = ""
+        temp = self.top
+        while temp is not None:
+            output += f"{temp.data} -> "
+            temp = temp.next
+        output += "None"
+        return output
+    
+    def __del__(self):
+        while self.top is not None:
+            t = self.top
+            self.top = t.next
+            del t
+        
